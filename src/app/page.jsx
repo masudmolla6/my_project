d83cee208fd675a98e7bf8b0ef5ab1f778d8c908
masudmolla6 <masudmolla2937@gmail.com>
2026-06-03 18:48:05 +0600

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div>
+      <h2 className='text-center text-2xl text-blue-500'>Learning NextJs</h2>
+    </div>
+  );
+};
+
+export default page;
